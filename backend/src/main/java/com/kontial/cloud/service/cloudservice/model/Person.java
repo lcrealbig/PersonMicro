04dad7fb0b10273584555a9a2c062a8830c1af8b
Mateusz Parzys <mateuszparzys123@gmail.com>
2024-06-12 00:21:02 +0200

    Bringed up spring version, added h2,liquibase,jpa dependencies. Reworked Person record into an entity. Backend runs on 8080

--- backend/src/main/java/com/kontial/cloud/service/cloudservice/model/Person.java
+++ backend/src/main/java/com/kontial/cloud/service/cloudservice/model/Person.java
@@ -1,5 +1,14 @@
 package com.kontial.cloud.service.cloudservice.model;
 
-public record Person(String id, String name) {
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+
+
+@Entity
+@Data
+public final class Person {
+    private final String id;
+    private final String name;
 
 }
